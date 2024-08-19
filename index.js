a215b148db59e788aab5b2596ec0b80091135680
suhail afnan <suhailafnan00@gmail.com>
2024-08-19 21:53:13 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/Ecommerce")
+require('dotenv').config();
+;
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGODB_USER;
+   
+    await mongoose.connect(process.env.MONGODB_USER);
+    console.log("DB is connected");
+  } catch (error) {
+    console.error("Error connecting to DB:", error.message);
+  }
+};
+connectDB()
+console.log('MONGODB_USER:', process.env.MONGODB_USER);
+
 require("dotenv").config();
 const express=require("express");
 const app=express();
 const path=require('path')
 const userRoute = require('./routes/userRoute');
+
  //const adminRoute = require('./routes/adminRoute');
 const session = require('express-session');
 const adminRoute=require('./routes/adminRoute')
